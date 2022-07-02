0876323de28d0ddf6a2711cfa873510fa6e2b4b4
umarofz <webnajottalim@gmail.com>
2022-07-02 22:44:40 +0500

    Add

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var userName = prompt("Name");
+var userSurname = prompt("Surname");
+var userAge = prompt("Your age");
+var userEmail = prompt("Email");
+var userEducation = prompt("Your education level");
+var userPhone = prompt("Phone number");
+var userProfession = prompt("Your profession");
+var userCity = prompt("City");
+var userGender = prompt("Your gender");
+var userPassword = prompt("Password");
+
+console.log(`Name: ${userName}\nSurname:${userSurname}\nAge:${userAge}\nEmail${userEmail}\nEducation:${userEducation}\nPhone:${userPhone}\nProfession:${userProfession}\nCity${userCity}\nGender:${userGender}\nPassword:${userPassword}`);
